b7cb65bd2825a572ee5bb4ea5a8fd622fad3ab76
Brian <bstanton773@gmail.com>
2021-04-02 10:48:49 -0500

    Working form get/post

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,13 @@
 from app import app
-from flask import render_template
+from flask import render_template, request
 from app.forms import CityWeatherForm
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
     form = CityWeatherForm()
-    return render_template('index.html', form=form)
+    city_data = None
+    if request.method == 'POST' and form.validate():
+        city = form.city_name.data
+        print(city)
+        city_data = city
+    return render_template('index.html', form=form, city_data=city_data)
